67d719cf61728b7a01a0f060fc2e2845b2d1dbbb
michaelsoft <michaellyk@qq.com>
2019-04-01 00:11:07 +0800

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using Actor1.Interfaces;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (var i=1; i<=10; i++)
+            //{
+            //    //ActorId id = ActorId.CreateRandom();
+            //    var id = new ActorId(i);
+            //    var url = "fabric:/MyActorApp";
+            //    //var url = "http://localhost:19081/MyActorApp";
+            //    var actor = ActorProxy.Create<IActor1>(id, url);
+
+            //    actor.SetCountAsync(100).GetAwaiter().GetResult();
+            //    var val = actor.GetCountAsync().GetAwaiter().GetResult();
+
+            //    Console.WriteLine(i.ToString() + ": " + val);
+            //}
+
+            Worker.DoWork();
+
+            Console.Read();
+        }
+    }
+}
---
+++ ConsoleApp1/Worker.cs
@@ -0,0 +1,34 @@
+﻿using Actor1.Interfaces;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Worker
+    {
+       private static string url = "fabric:/MyActorApp";
+
+        public static async void DoWork()
+        {
+            System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
+
+            for (var i = 1; i <= 10; i++)
+            {
+                var id = new ActorId(i);
+                
+                var actor = ActorProxy.Create<IActor1>(id, url);
+                
+                await actor.DoWorkAsync();
+
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed.Seconds);
+        }
+    }
+}
